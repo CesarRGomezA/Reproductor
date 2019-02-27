2fda3af79e23a1268defd343c829f2929baceeed
CesarRGomezA <CesarRGomezA@gmail.com>
2019-02-27 11:52:03 -0700

    Agregaos funcionalidad de slider

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -33,6 +33,8 @@ namespace Reproductor
 
         DispatcherTimer timer;
 
+        bool dragging = false;
+
         public MainWindow()
         {
 
@@ -46,6 +48,8 @@ namespace Reproductor
             timer.Interval = TimeSpan.FromMilliseconds(500);
             timer.Tick += Timer_Tick;
 
+
+
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -53,6 +57,11 @@ namespace Reproductor
             if (reader != null)
             {
                 lblTiempoInicial.Text = reader.CurrentTime.ToString().Substring(0, 8);
+                sldReproduccion.Value = reader.CurrentTime.TotalSeconds;
+                if (!dragging)
+                {
+                    sldReproduccion.Value = reader.CurrentTime.TotalSeconds;
+                }
             }
         }
 
@@ -111,6 +120,10 @@ namespace Reproductor
                 lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0, 8);
                 lblTiempoInicial.Text = reader.CurrentTime.ToString().Substring(0, 8);
 
+                sldReproduccion.Maximum = reader.TotalTime.TotalSeconds;
+                sldReproduccion.Value = reader.CurrentTime.TotalSeconds;
+
+
 
                 timer.Start();
             }
@@ -143,6 +156,20 @@ namespace Reproductor
                 btnReproducir.IsEnabled = true;
                 btnPausa.IsEnabled = false;
                 btnDetener.IsEnabled = false;
+            }
+        }
+
+        private void sldReproduccion_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            dragging = true;
+        }
+
+        private void sldReproduccion_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            dragging = false;
+            if (reader != null && output != null &&(output.PlaybackState != PlaybackState.Stopped))
+            {
+                reader.CurrentTime = TimeSpan.FromSeconds(sldReproduccion.Value);
             }
         }
     }
